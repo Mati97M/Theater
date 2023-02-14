47e348bec074a3f8616c2281bade92e78b6630ca
Mateusz Mieckowski <MMieckowski@wp.pl>
2023-02-14 19:47:38 +0100

    theater with reserving seats system (using ArrayList)

---
+++ src/dev/mati/Main.java
@@ -0,0 +1,23 @@
+package dev.mati;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println((char) ('A' + 4));
+
+        Theater theater = new Theater("ROMA musical theater",15,20);
+        //theater.getSeats();
+
+        reserveSeat(theater,"H11");
+        reserveSeat(theater,"H11");
+        reserveSeat(theater,"H80");
+
+    }
+
+    private static void reserveSeat(Theater theater, String seat) {
+        if(theater.reserveSeat(seat)) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is already taken, or does not exist");
+        }
+    }
+}
